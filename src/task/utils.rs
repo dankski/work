6e664c4ed116d942d251529c1d2cc70d0be11dec
Giacomo Amoroso <giacomo@amoroso.me>
2020-06-19 23:07:27 +0200

    TASK-00: Improve Error Message For Invalid Characters In Time

--- src/task/utils.rs
+++ src/task/utils.rs
@@ -1,13 +1,15 @@
 extern crate chrono;
 
 use chrono::NaiveTime;
+use std::error::Error;
 
-pub fn time_hm (time: &String) -> NaiveTime {
+pub fn time_hm(time: &String) -> NaiveTime {
   match NaiveTime::parse_from_str(time, "%H:%M") {
     Ok(NaiveTime) => NaiveTime,
-    Err(error) => {
-      panic!("There was a problem parsing the time: {:?}", error)
-    },
+    Err(error) => panic!(
+      "There was a problem parsing the time: {}",
+      error.to_string()
+    ),
   }
 }
 
@@ -16,17 +18,23 @@ mod tests {
   use super::*;
 
   #[test]
-  fn should_parse_naive_time () {
+  #[should_panic]
+  fn should_panic_on_invalid_characters() {
+    let time = String::from("15.30");
+    time_hm(&time);
+  }
+
+  #[test]
+  fn should_parse_naive_time() {
     let time = String::from("15:30");
     assert_eq!(time_hm(&time), NaiveTime::from_hms(15, 30, 0));
   }
 
   #[test]
-  fn should_return_time_span () {
+  fn should_return_time_span() {
     let start = time_hm(&String::from("15:30"));
     let end = time_hm(&String::from("16:01"));
     let span = end.signed_duration_since(start);
-    
     assert_eq!(span.num_minutes(), 31);
   }
 }
