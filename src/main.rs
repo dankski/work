8ec1fa8c35d821cd8e26a35e9e980a289b778892
Giacomo Amoroso <giacomo@amoroso.me>
2020-06-21 13:37:35 +0200

    BUGFIX-00: Fix Panic For Improper Handling Of Newline

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,14 @@ use std::io::prelude::*;
 use work::task::activity::*;
 use work::task::work::Day;
 
-
-fn filename (args: &[String]) -> Option<&str> {
+fn filename(args: &[String]) -> Option<&str> {
     if args.len() == 2 {
         return Some(&args[1]);
     }
     return None;
 }
 
-fn help () -> String {
+fn help() -> String {
     String::from("Missing filename! Please invoke: $ ./work [filename]")
 }
 
@@ -30,9 +29,14 @@ fn main() {
 
     let mut file = File::open(fname.unwrap()).expect("file not found!");
     let mut content = String::new();
-    file.read_to_string(&mut content).expect("something went wrong reading the file.");
+    file.read_to_string(&mut content)
+        .expect("something went wrong reading the file.");
 
-    let _activities: Vec<Activity> = content.lines().map(|l| work::task::activity::parse_activity_line(&l)).collect();
+    let _activities: Vec<Activity> = content
+        .trim_end_matches('\n')
+        .lines()
+        .map(|l| work::task::activity::parse_activity_line(&l))
+        .collect();
     let mut day = Day::new();
 
     day.load(_activities);
